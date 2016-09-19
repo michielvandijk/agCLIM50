34f6d972fc22043d5023ae9fb95d058c1659ecbb
Michiel van Dijk <michiel.vandijk@wur.nl>
2016-09-19 11:19:59 +0200

    sevilla plots

--- Code/PlotModelResults.R
+++ Code/PlotModelResults.R
@@ -6,7 +6,7 @@
 # `````````````````````````````````````````````````````````````````````````````````````````````````` 
 
 # PACKAGES
-BasePackages <- c("readr", "readxl", "foreign", "stringr", "gdata", "car", "zoo", "tidyr", "RColorBrewer", "dplyr", "ggplot2", "openxlsx", "scales", "lazyeval", "ggthemes", "scales")
+BasePackages <- c("readr", "readxl", "foreign", "stringr", "gdata", "car", "zoo", "tidyr", "RColorBrewer", "dplyr", "ggplot2", "openxlsx", "scales", "lazyeval", "ggthemes", "scales", "forcats")
 lapply(BasePackages, library, character.only = TRUE)
 #SpatialPackages <- c("rgdal", "gdalUtils", "ggmap", "raster", "rasterVis", "rgeos", "sp", "mapproj", "maptools", "proj4")
 #lapply(SpatialPackages, library, character.only = TRUE)
@@ -27,29 +27,34 @@ options(scipen=99) # surpress scientific notation
 options("stringsAsFactors"=FALSE) # ensures that characterdata that is loaded (e.g. csv) is not turned into factors
 options(digits=2)
 
-## COMPARE ALL
-# read data
+# ## COMPARE ALL
+# # read data
 TOTAL <- read.csv(file.path(dataPath, "ModelResults\\TOTAL_2016-09-14.csv"))
+TOTAL <- TOTAL %>% 
+  mutate(scenario = fct_relevel(scenario, 
+                                c("SSP1_NoCC", "SSP1_CC6", "SSP1_NoCC_m", "SSP1_CC26_m",
+                                  "SSP2_NoCC", "SSP2_CC6", "SSP2_NoCC_m", "SSP2_CC26_m",
+                                  "SSP3_NoCC", "SSP3_CC6", "SSP3_NoCC_m", "SSP3_CC26_m")))
 xtabs(~ variable + model, data = TOTAL)
-
-# create df with index observations for more than one model
-# NOT WORKING YET
-# TOTALsel <- TOTAL %>%
-#   group_by(year, scenario, region, item, variable) %>%
-#   filter(length(model) == 2)
+ 
+# # create df with index observations for more than one model
+# # NOT WORKING YET
+# # TOTALsel <- TOTAL %>%
+# #   group_by(year, scenario, region, item, variable) %>%
+# #   filter(length(model) == 2)
+# # 
+# # xtabs(~ model + variable, data = TOTALsel)
 # 
-# xtabs(~ model + variable, data = TOTALsel)
-
-# Line plot - index
-TOTAL_lineplot_i <- TOTAL %>%
-  group_by(variable, item) %>%
-  select(-value) %>%
-  rename(value = index) %>%
-  do(plots = lineplot_f(., "Index"))
-
-pdf(file = file.path(dataPath, "Graphs/TOTAL.pdf"), width = 12, height = 7)
-TOTAL_lineplot_i$plots
-dev.off()
+# # Line plot - index
+# TOTAL_lineplot_i <- TOTAL %>%
+#   group_by(variable, item) %>%
+#   select(-value) %>%
+#   rename(value = index) %>%
+#   do(plots = lineplot_f(., "Index"))
+# 
+# pdf(file = file.path(dataPath, "Graphs/TOTAL.pdf"), width = 12, height = 7)
+# TOTAL_lineplot_i$plots
+# dev.off()
 
 # Comparison GDP, POP and YEXO
 GDP_POP_YEXO <- TOTAL %>%
@@ -69,10 +74,20 @@ GDP_POP_YEXO_lineplot_i$plots
 dev.off()
 
 # BAR GRAPHS for WLD
+# Create colours
+red <- colorRampPalette(c("orange", "darkred"))
+blue <- colorRampPalette(c("skyblue", "darkblue"))
+green <- colorRampPalette(c("greenyellow", "darkgreen"))
+colour <- c(green(4), blue(4), red(4))
+names(colour) <- levels(TOTAL$scenario)
+
+
+# Function to plot bar graph
 barplot_f <- function(df, var, itm){
   df <- filter(df, variable == var, item == itm)
   title <- paste(var, itm, sep = "_")
   p = ggplot(data = df, aes(x = scenario, y = index, fill = scenario)) +
+    scale_fill_manual(values = colour) +
     geom_bar(stat="identity", colour = "black") + 
     facet_wrap(~model) +
     ggtitle(title) +
@@ -81,11 +96,13 @@ barplot_f <- function(df, var, itm){
   p
 }
 
+
+# Create database for plotting
 TOTAL_WLD <- filter(TOTAL, region == "WLD", year == 2050)
 xtabs(~variable + unit, data = TOTAL_WLD)
 
+# Create pdf
 pdf(file = file.path(dataPath, "Graphs/Sevilla.pdf"), width = 12, height = 7)
-
 barplot_f(TOTAL_WLD, "XPRP", "AGR")
 barplot_f(TOTAL_WLD, "XPRP", "CRP")
 barplot_f(TOTAL_WLD, "XPRP", "LSP")
@@ -147,6 +164,30 @@ barplot_f(TOTAL_WLD, "YEXO", "CRP")
 barplot_f(TOTAL_WLD, "NETT", "AGR")
 barplot_f(TOTAL_WLD, "NETT", "CRP")
 barplot_f(TOTAL_WLD, "NETT", "LSP")
+barplot_f(TOTAL_WLD, "NETT", "WHT")
+barplot_f(TOTAL_WLD, "NETT", "CGR")
+barplot_f(TOTAL_WLD, "NETT", "DRY")
+barplot_f(TOTAL_WLD, "NETT", "NRM")
+barplot_f(TOTAL_WLD, "NETT", "RIC")
+barplot_f(TOTAL_WLD, "NETT", "RUM")
+
+barplot_f(TOTAL_WLD, "IMPO", "AGR")
+barplot_f(TOTAL_WLD, "IMPO", "CRP")
+barplot_f(TOTAL_WLD, "IMPO", "LSP")
+barplot_f(TOTAL_WLD, "IMPO", "WHT")
+barplot_f(TOTAL_WLD, "IMPO", "CGR")
+barplot_f(TOTAL_WLD, "IMPO", "DRY")
+barplot_f(TOTAL_WLD, "IMPO", "NRM")
+barplot_f(TOTAL_WLD, "IMPO", "RIC")
+
+barplot_f(TOTAL_WLD, "EXPO", "AGR")
+barplot_f(TOTAL_WLD, "EXPO", "CRP")
+barplot_f(TOTAL_WLD, "EXPO", "LSP")
+barplot_f(TOTAL_WLD, "EXPO", "WHT")
+barplot_f(TOTAL_WLD, "EXPO", "CGR")
+barplot_f(TOTAL_WLD, "EXPO", "DRY")
+barplot_f(TOTAL_WLD, "EXPO", "NRM")
+barplot_f(TOTAL_WLD, "EXPO", "RIC")
 
 barplot_f(TOTAL_WLD, "CONS", "AGR")
 barplot_f(TOTAL_WLD, "CONS", "CRP")
@@ -163,16 +204,25 @@ barplot_f(TOTAL_WLD, "EMIS", "AGR")
 barplot_f(TOTAL_WLD, "EMIS", "CRP")
 barplot_f(TOTAL_WLD, "EMIS", "LSP")
 
+barplot_f(TOTAL_WLD, "ECO2", "TOT")
+#barplot_f(TOTAL_WLD, "ECO2", "AGR")
+#barplot_f(TOTAL_WLD, "ECO2", "CRP")
+#barplot_f(TOTAL_WLD, "ECO2", "LSP")
+
+barplot_f(TOTAL_WLD, "ECH4", "TOT")
+barplot_f(TOTAL_WLD, "ECH4", "AGR")
+barplot_f(TOTAL_WLD, "ECH4", "CRP")
+barplot_f(TOTAL_WLD, "ECH4", "LSP")
+
 barplot_f(TOTAL_WLD, "GDPT", "TOT")
 barplot_f(TOTAL_WLD, "POPT", "TOT")
 dev.off()
 
-
-
 xtabs(~variable + unit, data = MAgPIE)
 # bar graph - index
 
-df <- filter(TOTAL_WLD, variable == "XPRP", item == "CRP")
+##############################
+# Part below is from FOODSECURE, not adapted yet to agCLIM50
 
 # Comparison of consumption
 CONS <- TOTAL2 %>% 
